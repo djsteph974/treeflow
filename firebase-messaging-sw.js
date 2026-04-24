80dc12bb7fcf9ce8dc33eb1f470c62b87038f5f0
djsteph974 <113823731+djsteph974@users.noreply.github.com>
2026-04-24 15:15:08 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,23 +1,12 @@
-importScripts('https://www.gstatic.com/firebasejs/12.12.0/firebase-app-sw.js');
-importScripts('https://www.gstatic.com/firebasejs/12.12.0/firebase-messaging-sw.js');
-
-firebase.initializeApp({
-    apiKey: "AIzaSyA8K0QUGr3xUSra2q_feHGrAjt0qAfrKY8",
-    authDomain: "treeflow-music.firebaseapp.com",
-    projectId: "treeflow-music",
-    messagingSenderId: "330986852150",
-    appId: "1:330986852150:web:d57958930fa75b9504ad97"
-});
-
-const messaging = firebase.messaging();
-
-// Gère l'affichage quand on reçoit la notif en arrière-plan
-messaging.onBackgroundMessage((payload) => {
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: 'https://lh3.googleusercontent.com/d/1vsktKnp28H1JwhJeORR-UJsuaT-UItjI'
-    };
-
-    self.registration.showNotification(notificationTitle, notificationOptions);
+getToken(messaging, { vapidKey: 'BIUhj36lFGfQTI7Ifiv65s31oTQcWvePTkxExIf6fVZ-KC5o7tlV8DovqAPqYdbHDWm9_TxR6QVzGQHL9OrRzQ' })
+.then((currentToken) => {
+    if (currentToken) {
+        // C'est ce message qu'on veut voir !
+        alert("TOKEN REÇU :\n\n" + currentToken);
+        console.log("Token:", currentToken);
+    } else {
+        alert("Permission accordée, mais aucun token généré. Réessaie de rafraîchir la page.");
+    }
+}).catch((err) => {
+    alert("Erreur lors de la récupération : " + err);
 });
